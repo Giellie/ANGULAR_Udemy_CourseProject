ac8af20d791ec0c03e64746416fd83d93af77a09
Guilleame Maritz <giellie19733@gmail.com>
2019-09-15 22:56:22 +0200

    Components Create - NavBar is broken :(

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  myNumber: number;
+  myString: number;
+
+  constructor() {
+    this.myNumber = 6;
+    this.myString = 6;
+
+    const moo = this.myNumber === this.myString ? 'moo' : 'bla';
+  }
+
+  ngOnInit() {
+  }
+}
+
+
+
+// TESTING VS EXTENSION - json2ts
+// export interface Batter {
+// 	id: string;
+// 	type: string;
+// }
+
+// export interface Batters {
+// 	batter: Batter[];
+// }
+
+// export interface Topping {
+// 	id: string;
+// 	type: string;
+// }
+
+// export interface RootObject {
+// 	id: string;
+// 	type: string;
+// 	name: string;
+// 	ppu: number;
+// 	batters: Batters;
+// 	topping: Topping[];
+// }
