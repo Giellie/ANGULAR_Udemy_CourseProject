47c3c113cbfcea2755ea5c2a484d86266a5d2937
Guilleame Maritz <giellie19733@gmail.com>
2019-09-18 23:22:57 +0200

    Section 3 - DONE

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Pasta', 'Supper yummy italian pizza', './assets/images/recipe1.jpg'),
+    new Recipe('Chicken', 'Roasted chicken with green beans', './assets/images/recipe2.jpg'),
+    new Recipe('Pizza', 'Cheesy pepperoni deep dish pizza', './assets/images/recipe3.jpg')
+  ];
 
   constructor() { }
 
